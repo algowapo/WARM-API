3f71bf50337fe74273f3adcaf5e15d092fe81893
Juan Carlos Hernandez <you@example.com>
2022-05-02 22:04:46 -0500

    Initial commit

---
+++ src/main/java/com/warm/WarmApplication.java
@@ -0,0 +1,13 @@
+package com.warm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/warm/WarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
