4c0773eb2c957105ec8c154571d50f251dda0783
Juan Carlos Hernández <39997847+Juanca0312@users.noreply.github.com>
2022-05-02 23:10:38 -0500

    ✨ Implemented model User

--- src/main/java/com/warm/models/User.java
+++ src/main/java/com/warm/models/User.java
@@ -1,4 +1,23 @@
 package com.warm.models;
 
+import lombok.Data;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @OneToOne(mappedBy = "user")
+    private Profile profile;
 }
