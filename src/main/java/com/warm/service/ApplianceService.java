f71816302900818eda0c9698d869877e55394348
cjmazzarri <cjmazzarri@gmail.com>
2022-05-04 22:15:24 -0500

    ✨Added update and delete methods

--- src/main/java/com/warm/service/ApplianceService.java
+++ src/main/java/com/warm/service/ApplianceService.java
@@ -2,8 +2,11 @@ package com.warm.service;
 
 import com.warm.models.Appliance;
 import com.warm.resource.ApplianceRequest;
+import org.springframework.http.ResponseEntity;
 
 public interface ApplianceService {
     Appliance create(ApplianceRequest appliance);
     Appliance findById(Long id);
+    Appliance update(Long id, ApplianceRequest appliance);
+    ResponseEntity<?> delete(Long id);
 }
--- src/main/java/com/warm/service/ApplianceServiceImpl.java
+++ src/main/java/com/warm/service/ApplianceServiceImpl.java
@@ -6,6 +6,7 @@ import com.warm.models.Appliance;
 import com.warm.repository.ApplianceRepository;
 import com.warm.resource.ApplianceRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,13 +24,32 @@ public class ApplianceServiceImpl implements ApplianceService {
     }
 
     @Override
+    public Appliance update(Long id, ApplianceRequest applianceReq) {
+        return applianceRepository.findById(id).map(appliance -> {
+            appliance.setName(applianceReq.getName());
+            appliance.setBrand(applianceReq.getBrand());
+            appliance.setModel(applianceReq.getModel());
+            appliance.setType(applianceReq.getType());
+            return applianceRepository.save(appliance);
+        }).orElseThrow(() -> new ServiceException(Error.APPLIANCE_NOT_FOUND));
+    }
+
+    @Override
+    public ResponseEntity<?> delete(Long id) {
+        return applianceRepository.findById(id).map(appliance -> {
+            applianceRepository.delete(appliance);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ServiceException(Error.APPLIANCE_NOT_FOUND));
+    }
+
+    @Override
     public Appliance create(ApplianceRequest request) {
         Appliance appliance = fromReq(request);
         appliance.setUser(userService.findById(request.getUserId()));
         return applianceRepository.save(appliance);
     }
 
-
+    //mapper
     private Appliance fromReq(ApplianceRequest request) {
         Appliance appliance;
         if (request.getId() != null) {
